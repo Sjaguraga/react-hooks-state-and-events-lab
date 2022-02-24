be980f272f9ebf7d544da451396aceef1d5cc9cf
serref jaguraga <reef163@gmail.com>
2022-02-24 11:54:18 -0500

    commit tree 79631ec6e58ef6cdd16c1a52a13efa6d7f93b548

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemCart, SetItemCart] = useState(true);
+
+  function inItemCart() {
+    SetItemCart((itemCart) => !itemCart);
+  }
+  const itemClass = itemCart ? "Add to Cart" : "Remove From Cart";
+  const inCart = itemCart ? "" : "in-cart";
+  const color = itemCart ? "yellow" : "Hotpink";
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        style={{ background: color }}
+        className={itemClass}
+        onClick={inItemCart}
+      >
+        {itemCart ? "Add to Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
